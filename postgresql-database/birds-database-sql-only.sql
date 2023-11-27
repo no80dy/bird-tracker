ce4b53029bf0dcf961da1dcf4716e9017609cf00
no80dy <no8odyas@gmail.com>
2023-11-27 23:16:40 +0300

    Add sql-only file to demonstrate pgsql language for birds_database

--- postgresql-database/birds-database-sql-only.sql
+++ postgresql-database/birds-database-sql-only.sql
@@ -71,3 +71,61 @@ CREATE TABLE IF NOT EXISTS content.bird_images (
     created_at TIMESTAMP WITH TIME ZONE,
     updated_at TIMESTAMP WITH TIME ZONE
 );
+
+-- Set time for created_at and updated_at automatically before INSERT, UPDATE
+CREATE OR REPLACE FUNCTION content.update_timestamps()
+RETURNS TRIGGER AS $$
+BEGIN
+    NEW.updated_at = NOW();
+    IF TG_OP = 'INSERT' THEN
+       NEW.created_at = NOW();
+    END IF;
+
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+-- Create the hash_password function
+CREATE EXTENSION IF NOT EXISTS pgcrypto;
+CREATE OR REPLACE FUNCTION content.hash_password()
+RETURNS TRIGGER AS $$
+BEGIN
+    NEW.password_hash := crypt(NEW.password_hash, gen_salt('bf'));
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+-- Create the hash_password_trigger trigger
+CREATE TRIGGER hash_password
+BEFORE INSERT ON content.users
+FOR EACH ROW EXECUTE FUNCTION content.hash_password();
+
+CREATE TRIGGER bird_statuses_timestamp
+BEFORE INSERT OR UPDATE ON content.bird_statuses
+FOR EACH ROW EXECUTE FUNCTION content.update_timestamps();
+
+CREATE TRIGGER bird_families_timestamp
+BEFORE INSERT OR UPDATE ON content.bird_families
+FOR EACH ROW EXECUTE FUNCTION content.update_timestamps();
+
+CREATE TRIGGER bird_locations_timestamp
+BEFORE INSERT OR UPDATE ON content.bird_locations
+FOR EACH ROW EXECUTE FUNCTION content.update_timestamps();
+
+CREATE TRIGGER users_timestamp
+BEFORE INSERT OR UPDATE ON content.users
+FOR EACH ROW EXECUTE FUNCTION content.update_timestamps();
+
+CREATE TRIGGER birds_timestamp
+BEFORE INSERT OR UPDATE ON content.birds
+FOR EACH ROW EXECUTE FUNCTION content.update_timestamps();
+
+CREATE TRIGGER bird_observations_timestamp
+BEFORE INSERT OR UPDATE ON content.bird_observations
+FOR EACH ROW EXECUTE FUNCTION content.update_timestamps();
+
+CREATE TRIGGER bird_images_timestamp
+BEFORE INSERT OR UPDATE ON content.bird_images
+FOR EACH ROW EXECUTE FUNCTION content.update_timestamps();
+
+INSERT INTO content.users (id, username, email, password_hash) VALUES ('0b469d87-82cd-4544-a01e-8d99c167a81f', 'user', 'user@example.com', 'Gfhjkm0995')
--- postgresql-database/postgresql.Dockerfile
+++ postgresql-database/postgresql.Dockerfile
@@ -1,4 +1,3 @@
 FROM postgres:16-alpine
 
-ENV POSTGRES_DB=birds_database
-COPY birds-database.sql /docker-entrypoint-initdb.d/birds-database.sql
+COPY birds-database.sql /docker-entrypoint-initdb.d/

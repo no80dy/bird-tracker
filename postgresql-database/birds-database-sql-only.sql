5dd454efb7ed6557150081069bdf4c099a893c19
no80dy <no8odyas@gmail.com>
2023-12-16 20:51:00 +0300

    Delete all DDL for file birds-database-sql-only.sql

--- postgresql-database/birds-database-sql-only.sql
+++ postgresql-database/birds-database-sql-only.sql
@@ -1,77 +1,3 @@
-CREATE SCHEMA IF NOT EXISTS content;
-
-SET search_path TO public,content;
-
-CREATE TABLE IF NOT EXISTS content.bird_statuses (
-    id UUID PRIMARY KEY,
-    status_name VARCHAR(30) NOT NULL,
-
-    created_at TIMESTAMP WITH TIME ZONE,
-    updated_at TIMESTAMP WITH TIME ZONE
-);
-
-CREATE TABLE IF NOT EXISTS content.bird_families (
-    id UUID PRIMARY KEY,
-    family_name VARCHAR(30) NOT NULL,
-    description TEXT,
-
-    created_at TIMESTAMP WITH TIME ZONE,
-    updated_at TIMESTAMP WITH TIME ZONE
-);
-
-CREATE TABLE IF NOT EXISTS content.bird_locations(
-    id UUID PRIMARY KEY,
-    longitude FLOAT,
-    latitude FLOAT,
-
-    created_at TIMESTAMP WITH TIME ZONE,
-    updated_at TIMESTAMP WITH TIME ZONE
-);
-
-CREATE TABLE IF NOT EXISTS content.users (
-    id UUID PRIMARY KEY,
-    username VARCHAR(30) NOT NULL,
-    email VARCHAR(30) NOT NULL,
-    password_hash VARCHAR(255) NOT NULL,
-
-    created_at TIMESTAMP WITH TIME ZONE,
-    updated_at TIMESTAMP WITH TIME ZONE
-);
-
-CREATE TABLE IF NOT EXISTS content.birds (
-    id UUID PRIMARY KEY,
-    bird_name VARCHAR(30) NOT NULL,
-    scientific_name VARCHAR(30),
-    description TEXT,
-    status_id UUID NOT NULL REFERENCES content.bird_statuses (id) ON DELETE CASCADE,
-    family_id UUID NOT NULL REFERENCES content.bird_families (id) ON DELETE CASCADE,
-
-    created_at TIMESTAMP WITH TIME ZONE,
-    updated_at TIMESTAMP WITH TIME ZONE
-);
-
-CREATE TABLE IF NOT EXISTS content.bird_observations (
-    id UUID PRIMARY KEY,
-    observation_name VARCHAR(30) NOT NULL,
-    location_id UUID NOT NULL REFERENCES content.bird_locations (id) ON DELETE CASCADE,
-    bird_id UUID NOT NULL REFERENCES content.birds (id) ON DELETE CASCADE,
-    user_id UUID REFERENCES content.users (id) ON DELETE CASCADE,
-    gender TEXT NOT NULL,
-    description TEXT,
-
-    created_at TIMESTAMP WITH TIME ZONE,
-    updated_at TIMESTAMP WITH TIME ZONE
-);
-
-CREATE TABLE IF NOT EXISTS content.bird_images (
-    id UUID PRIMARY KEY,
-    observation_id UUID NOT NULL REFERENCES content.bird_observations (id) ON DELETE CASCADE,
-    image TEXT NOT NULL,
-
-    created_at TIMESTAMP WITH TIME ZONE,
-    updated_at TIMESTAMP WITH TIME ZONE
-);
-
 -- Set time for created_at and updated_at automatically before INSERT, UPDATE
 CREATE OR REPLACE FUNCTION content.update_timestamps()
 RETURNS TRIGGER AS $$
@@ -87,6 +13,7 @@ $$ LANGUAGE plpgsql;
 
 -- Create the hash_password function
 CREATE EXTENSION IF NOT EXISTS pgcrypto;
+
 CREATE OR REPLACE FUNCTION content.hash_password()
 RETURNS TRIGGER AS $$
 BEGIN
